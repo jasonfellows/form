dbd56312f5c6a2741512de6250d6c64299373ffb
jasonfellows <jasonfellows@gmail.com>
2018-01-25 23:14:13 +0100

    Map recipient fields to components and render

--- client/src/components/Form.js
+++ client/src/components/Form.js
@@ -1,29 +1,58 @@
 import React, { Component } from 'react';
 import DateTimePicker from 'material-ui-datetimepicker';
 import DatePickerDialog from 'material-ui/DatePicker/DatePickerDialog';
+import map from 'lodash/map'
+import SelectField from 'material-ui/SelectField';
+import TextField from 'material-ui/TextField';
 import TimePickerDialog from 'material-ui/TimePicker/TimePickerDialog';
 import './Form.css';
 
 class Form extends Component {
-  constructor(props) {
+  constructor (props) {
     super(props);
     this.state = {
       delivery_at: null,
       recipient: {
-        name: null,
-        street: null,
-        city: null,
-        state: null,
-        country: null,
-        zipcode: null,
-        phone: null
+        name: "",
+        street: "",
+        city: "",
+        state: "",
+        country: "",
+        zipcode: "",
+        phone: ""
       }
     };
+
+    this.fieldToComponent = this.fieldToComponent.bind(this);
+    this.setRecipientData = this.setRecipientData.bind(this);
+  }
+
+  fieldToLabel (field) {
+    return `Recipient ${field.charAt(0).toUpperCase() + field.substring(1).toLowerCase()}`;
+  }
+
+  fieldToComponent (field) {
+    if (field === "country") {
+      return <SelectField
+        key={field}
+      />
+    } else {
+      return <TextField
+        floatingLabelText={this.fieldToLabel(field)}
+        key={field}
+        onChange={this.setRecipientData}
+        value={this.state.recipient[field]}
+      />
+    }
   }
 
   setDeliveryAt = (dateTime) => this.setState({ delivery_at: dateTime })
 
-  render() {
+  setRecipientData () {
+    return null;
+  }
+
+  render () {
     return (
       <div className="Form">
         <DateTimePicker
@@ -39,7 +68,7 @@ class Form extends Component {
           TimePicker={TimePickerDialog}
           value={this.state.delivery_at}
         />
-
+        {map(Object.keys(this.state.recipient), this.fieldToComponent)}
       </div>
     );
   }
