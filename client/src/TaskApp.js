0ccfcabed9944ee3876c7bb5a1a800e7b4c1b3c4
jasonfellows <jasonfellows@gmail.com>
2018-01-25 21:51:06 +0100

    Create Form component

--- client/src/TaskApp.js
+++ client/src/TaskApp.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import Form from './components/Form'
 import './TaskApp.css';
 
 class TaskApp extends Component {
@@ -7,6 +8,7 @@ class TaskApp extends Component {
     return (
       <MuiThemeProvider>
         <div className="TaskApp">
+          <Form />
         </div>
       </MuiThemeProvider>
     );
---
+++ client/src/components/Form.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './Form.css';
+
+class Form extends Component {
+  render() {
+    return (
+      <div className="Form">
+      </div>
+    );
+  }
+}
+
+export default Form;
