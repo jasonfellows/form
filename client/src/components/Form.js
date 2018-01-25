56626b6b17fe7fb3bb68e1db9399af2aeea30a32
jasonfellows <jasonfellows@gmail.com>
2018-01-25 22:19:49 +0100

    Add delivery_at input and state

--- client/src/components/Form.js
+++ client/src/components/Form.js
@@ -1,10 +1,35 @@
 import React, { Component } from 'react';
+import DateTimePicker from 'material-ui-datetimepicker';
+import DatePickerDialog from 'material-ui/DatePicker/DatePickerDialog';
+import TimePickerDialog from 'material-ui/TimePicker/TimePickerDialog';
 import './Form.css';
 
 class Form extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      delivery_at: null
+    };
+  }
+
+  setDeliveryAt = (dateTime) => this.setState({ delivery_at: dateTime })
+
   render() {
     return (
       <div className="Form">
+        <DateTimePicker
+          autoOkTimePicker={true}
+          clearIcon={null}
+          DatePicker={DatePickerDialog}
+          floatingLabelText="Delivery at"
+          format='YYYY-MM-DD HH:mm'
+          minDate={Date.now()}
+          name='picker'
+          onChange={this.setDeliveryAt}
+          timeFormat= '24hr'
+          TimePicker={TimePickerDialog}
+          value={this.state.delivery_at}
+        />
       </div>
     );
   }
