bac7d0a496dee903758e488b3d6857edce8a2d84
jasonfellows <jasonfellows@gmail.com>
2018-01-25 21:34:11 +0100

    Rename create-react-app boilerplate

---
+++ client/src/TaskApp.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './TaskApp.css';
+
+class TaskApp extends Component {
+  render() {
+    return (
+      <div className="TaskApp">
+      </div>
+    );
+  }
+}
+
+export default TaskApp;
--- client/src/TaskApp.test.js
+++ client/src/TaskApp.test.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import TaskApp from './TaskApp';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(<TaskApp />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
